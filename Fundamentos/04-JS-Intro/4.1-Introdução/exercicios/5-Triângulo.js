731664799ac648f671e275d21ddf422ad2e710a4
Otavio RL <otaviohlopess@gmail.com>
2022-03-07 20:45:57 -0300

    Mais exercicios

---
+++ Fundamentos/04-JS-Intro/4.1-Introdução/exercicios/5-Triângulo.js
@@ -0,0 +1,14 @@
+const a = 45
+const b = 45
+const c = 400
+
+
+if ((a || b || b) > 360 || (a || b || b) <= 0) {
+    console.log("Ângulo não é valido")
+}
+else if (a + b + c === 180) {
+    console.log("É um triângulo")
+}
+else {
+    console.log("Não é um triângulo")
+}
