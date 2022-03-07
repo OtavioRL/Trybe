ee52790db9f46a98c3a51f1e10df5f24b7eb876f
Otavio RL <otaviohlopess@gmail.com>
2022-03-07 15:23:04 -0300

    Correção da sintaxe

--- Fundamentos/04-JS-Intro/4.1-Introdução/var.js
+++ Fundamentos/04-JS-Intro/4.1-Introdução/var.js
@@ -1,8 +1,8 @@
-const myName = "Otavio"
-const birthCity = "São Paulo" 
-let birthYear = 2002
-birthYear = 2030
+const myName = "Otavio";
+const birthCity = "São Paulo";
+let birthYear = 2002;
+birthYear = 2030;
 
-console.log(myName)
-console.log(birthCity)
-console.log(birthYear)
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
