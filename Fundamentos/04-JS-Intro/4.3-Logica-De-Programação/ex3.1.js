d7fb43ed19e4297b7a2be222b921cc0dc1568b67
Otavio RL <otaviohlopess@gmail.com>
2022-03-10 12:53:58 -0300

    ex3.1 e ex3.2

---
+++ Fundamentos/04-JS-Intro/4.3-Logica-De-Programação/ex3.1.js
@@ -0,0 +1,13 @@
+let array = ["java", "javascript", "python", "html", "css"];
+
+for (let i = 0; i < array.length; i++) {
+  let soma = 0;  
+  for (let z = 0; z < array.length; z++) {
+    if (array[i].length > array[z].length) {
+      soma += 1;
+    }
+    if (soma === array.length - 1) {
+        console.log(array[i])
+    }
+  }
+}
