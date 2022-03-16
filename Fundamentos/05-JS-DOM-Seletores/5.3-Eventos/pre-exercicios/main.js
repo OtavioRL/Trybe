62c32f6b48305fdc67ea7f0969cef34afe98fa83
Otavio RL <otaviohlopess@gmail.com>
2022-03-16 18:45:04 -0300

    primeiro requisito

--- Fundamentos/05-JS-DOM-Seletores/5.3-Eventos/pre-exercicios/main.js
+++ Fundamentos/05-JS-DOM-Seletores/5.3-Eventos/pre-exercicios/main.js
@@ -1,15 +1,22 @@
-const firstLi = document.getElementById('first-li');
-const secondLi = document.getElementById('second-li');
-const thirdLi = document.getElementById('third-li');
-const input = document.getElementById('input');
-const myWebpage = document.getElementById('my-spotrybefy');
-
+const firstLi = document.getElementById("first-li");
+const secondLi = document.getElementById("second-li");
+const thirdLi = document.getElementById("third-li");
+const input = document.getElementById("input");
+const myWebpage = document.getElementById("my-spotrybefy");
 
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+
+let lis = document.querySelectorAll("li");
+for (let i = 0; i < lis.length; i += 1) {
+  lis[i].addEventListener("click", function () {
+    lis[i].className = "tech";
+  });
+}
+
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
@@ -24,15 +31,14 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // Segue abaixo um exemplo do uso de event.target:
 
-
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
+  event.target.innerText = "Opção reiniciada";
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
 }
 
-firstLi.addEventListener('dblclick', resetText);
+firstLi.addEventListener("dblclick", resetText);
 
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
