185452bf007d985a6b31222c98ec4fa824aa0ce9
Otavio RL <otaviohlopess@gmail.com>
2022-04-05 21:22:18 -0300

    parte 2 dos exercicios

--- Fundamentos/07-Javascript-ES6-Testes/7.3-Jest/exercicios/sum.test.js
+++ Fundamentos/07-Javascript-ES6-Testes/7.3-Jest/exercicios/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require("./sum");
+const {sum, myRemove} = require("./sum");
 
 describe("Testes da função sum", () => {
   test("Retorna 9 ao receber os parametros 4 & 5??", () => {
@@ -9,5 +9,16 @@ describe("Testes da função sum", () => {
   });
   test('Retorna um erro ao receber uma string', () => {
     expect(() => {sum(4, "5")}).toThrow(new Error('parameters must be numbers'));
+  });
+});
+describe("Testes da função myRemove", () => {
+  test("Retorna o array esperado após receber [1, 2, 3, 4] e 3", () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+  test("Não retorna [1, 2, 3, 4] ao receber o mesmo", () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  test("Retorna o array esperado após receber [1, 2, 3, 4] e 5", () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   });
 });
