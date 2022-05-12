9d689ac80cc0883912285703b045d500434abc48
Otavio RL <otaviohlopess@gmail.com>
2022-05-12 11:13:28 -0300

    start

--- Front-End/11-Estado-Eventos-Forms/11.2-Forms/myform-2.0/src/App.js
+++ Front-End/11-Estado-Eventos-Forms/11.2-Forms/myform-2.0/src/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <fieldset>
+            <label>Nome:</label>
+            <input />              
+          </fieldset>
+        </form>
+      </div>
+    );
+  }  
 }
 
 export default App;
