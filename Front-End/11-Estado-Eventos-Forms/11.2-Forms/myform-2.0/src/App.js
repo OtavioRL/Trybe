3cecbe4203ce8a53d723e16e1a43568dde648d9b
Otavio RL <otaviohlopess@gmail.com>
2022-06-01 14:58:14 -0300

    pendencias

--- Front-End/11-Estado-Eventos-Forms/11.2-Forms/myform-2.0/src/App.js
+++ Front-End/11-Estado-Eventos-Forms/11.2-Forms/myform-2.0/src/App.js
@@ -3,15 +3,86 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-
+    Nome: '',
+    Email: '',
+    Cpf: '',
+    Endereco: '',
+    Cidade: '',
   }
+
+  handleChange = (event) => {
+    const { target } = event;
+    const { value, name } = target;
+    this.setState({ [name]: value });
+    console.log(this.state);  
+  }  
+
   render() {
     return (
       <div className="App">
         <form>
           <fieldset>
             <label>Nome:</label>
-            <input />              
+            <input 
+            name='Nome' 
+            onChange={this.handleChange}
+            />
+
+            <label>Email:</label>
+            <input 
+            name='Email' 
+            onChange={this.handleChange}
+            />
+
+            <label>CPF:</label>
+            <input 
+            name='Cpf' 
+            onChange={this.handleChange}
+            />
+            <br />
+            <br />
+
+            <label>Endereço:</label>
+            <input 
+            name='Endereco' 
+            onChange={this.handleChange}
+            />
+
+            <label>Cidade:</label>
+            <input 
+            name='Cidade' 
+            onChange={this.handleChange}
+            />
+
+            <select>
+              <option value='SP'>São Paulo</option>
+              <option value='RJ'>Rio de janeiro</option>
+              <option value='PA'>Pará</option>
+              <option value='ES'>Espirito Santo</option>
+              <option value='MG'>Minas Gerais</option>
+              <option value='AM'>Amazonas</option>
+              <option value='RS'>Rio Grande do Sul</option>
+              <option value='SC'>Santa Catarina</option>
+              <option value=''>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+              <option value='x'>x</option>
+            </select>
+
           </fieldset>
         </form>
       </div>
